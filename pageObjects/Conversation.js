61c5f689f9cd111a9b0245feb1f0e4866d319555
ShreyaPatel41 <the-email-you-used-for-shreya@example.com>
2026-05-18 14:13:18 +0530

    Fix conversation locator property capitalization inside Conversation page object

--- pageObjects/Conversation.js
+++ pageObjects/Conversation.js
@@ -2,7 +2,7 @@ const { expect } = require('@playwright/test');
 class Conversation {
     constructor(page) {
         this.page = page;
-        this.Conversation = page.locator('xpath=/html/body/div[1]/div/div[1]/nav/button[4]');
+        this.conversation = page.locator('xpath=/html/body/div[1]/div/div[1]/nav/button[4]');
         this.pageTitle = page.locator('xpath=/html/body/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]')
         this.transcriptmodule = page.locator('xpath=/html/body/div[1]/div/div[3]/div/div/div[1]/div[2]/div/button[1]')
         this.livesessionmodule = page.locator('xpath=/html/body/div[1]/div/div[3]/div/div/div[1]/div[2]/div/button[2]')
