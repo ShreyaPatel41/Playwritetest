1cd525a7c884d9e239798d9b6cea64662dd1a159
ShreyaPatel41 <the-email-you-used-for-shreya@example.com>
2026-05-18 12:19:42 +0530

    Integrate Allure Premium Reporting Dashboard into Playwright configuration and CI/CD workflow

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. Limit to 1 worker because of persistent profile locks */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -32,6 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     video: 'on',
+    screenshot: 'on',
   },
 
   projects: [
